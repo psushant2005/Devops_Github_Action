cf1593ef5acaf83afd81842dbf3c5898552272c6
psushant2005 <110098237+psushant2005@users.noreply.github.com>
2026-04-25 20:53:49 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -2,7 +2,14 @@
 # This is a test commit
 def add(a, b):
     return a + b
+# commit for testing
+def sub(x, z);
+    return z - x
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+# commit for testing
+def test_sub():
+    assert sub(2, 3) == 1
+    assert sub(-3, 3) == 6
